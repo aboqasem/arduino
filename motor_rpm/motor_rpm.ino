f1c1d979c18db722ae789b8fe7e7d3b728a7ad6e
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2020-09-15 18:55:26 +0800

    Refactor motor_rpm.ino

--- motor_rpm/motor_rpm.ino
+++ motor_rpm/motor_rpm.ino
@@ -1,14 +1,15 @@
-const uint8_t PIN02 = 2;
-uint64_t start_time = 0;
-volatile int interrupts_count = 0;
-int motor_rpm = 0;
+#include <Arduino.h>
+
+volatile uint8_t interrupts_count = 0;
+volatile uint64_t start_time = 0;
+uint16_t motor_rpm = 0;
 
 // the setup function runs once on power or reset
 void setup() {
   // begin the serial for data transmission
   Serial.begin(9600);
-  // attach interrupt subroutine to pin 2 on rising
-  attachInterrupt(digitalPinToInterrupt(PIN02), [](){++interrupts_count;}, RISING);
+  // attach interrupt to increment interrupts_count on RISING signal on pin 2
+  attachInterrupt(digitalPinToInterrupt(2), [](){++interrupts_count;}, RISING);
 }
 
 // the loop function loops forever
@@ -18,7 +19,7 @@ void loop() {
   // reset number of interrupts
   interrupts_count = 0;
   // count interrupts for one second
-  while (millis() - start_time < 1000) {}
+  while (millis() - start_time < 1000);
   // the motor rounds per minutes =
   // interrupts / 2 (pulses per revolution) * 60 (seconds)
   motor_rpm = interrupts_count / 2 * 60;
